2f01bf89231ac9cff090d87a3675d75984d43df9
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-05 17:51:41 +0200

    user can favorite and unfavorite item. start logo

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Gunther Masi Haas on 05/06/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Gunther Masi Haas on 05/06/2024.
+//
+
+import Foundation
