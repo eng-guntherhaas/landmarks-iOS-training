dcc19a3addb241fe02efb14e427357fa4edbf9f2
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-06 10:19:13 +0200

    create badge logo

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Gunther Masi Haas on 05/06/2024.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
--- Landmarks/Views/HexagonParameters.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HexagonParameters.swift
-//  Landmarks
-//
-//  Created by Gunther Masi Haas on 05/06/2024.
-//
-
-import Foundation
